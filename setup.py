ff5433bcbf9edc0aaa4645c1108b06318e5e2a54
domeevans <91951444+domeevans@users.noreply.github.com>
2023-05-26 12:31:00 +0100

    Update setup.py with new version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='dom_tools',
-    version='0.0.3',
+    version='0.0.4',
     author='Dominic Evans',
     author_email='dominic.evans@decathlon.com',
     description='Testing installation of Package',
